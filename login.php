c563a40ab906b61192675cd49b8f225c49e040e5
Dilyan <dilyanatanasov177@gmail.com>
2020-09-29 20:56:51 +0300

    add new code

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require_once "auth.php";
+
+if (!empty($_POST) && isset($_POST["username"]) && !empty($_POST["username"])) {
+    // echo "Your username is ".$_POST["username"];
+} else {
+    echo "Username is not set yet!";
+}
+
+if (!empty($_POST) && isset($_POST["password"]) && !empty($_POST["password"])) {
+    //echo "<br>Your password is ".$_POST["password"];
+} else {
+    echo "Password is not set yet!";
+}
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+$auth = new Authentication();
+$auth->login($username, $password);
